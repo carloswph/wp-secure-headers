d0c5cc95c6f4ef609358868b94915c9361aa24a6
carlos <geral@meuppt.pt>
2021-02-26 18:32:52 +0000

    First review

--- src/Headers.php
+++ src/Headers.php
@@ -23,15 +23,15 @@ class Headers {
 		$this->site = site_url('/wp-content/security/', 'https');
 		$this->set('Expect-CT', 'max-age=0, report-uri=' . site_url("/wp-content/security/", "https"));
 
-    	add_filter('wp_headers', array($this, 'add'), 10, 1);
+    	action('send_headers', array($this, 'add'));
 
     }
 
-    protected function add($headers) {
+    protected function add() {
 
     	foreach ($this->toApply as $key => $value) {
     		
-    		$headers[$key] = $value;
+    		headers($key . ': ' . $value);
     	}
 
     	return $headers;
